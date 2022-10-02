d4937d59527426cb9318553000cfd48a970b33f1
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-10-02 17:41:59 -0600

    api note post command created and working. Still need to impliment it into the code

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
 const { readAndAppend } = require('../helpers/fsUtils');
-const noteData = require('../db/db.json')
+const noteData = require('../db/db.json');
+const uuid = require('../helpers/uuid')
 
 
 
@@ -8,5 +9,26 @@ notes.get('/', (req, res) => res.json(noteData))
 
 
 
+notes.post('/', (req, res) => {
+
+    const {title, note} = req.body
+    if (title && note) {
+        const newNote = {
+            title,
+            note,
+            note_Id: uuid(),
+        }
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote
+        };
+        res.json(response)
+    }
+    else {
+        res.json('Error in posting feedback');
+      }
+})
 
 module.exports = notes;
