8502b1accbd68c91e825567fbea21a3bf499513a
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-10-04 21:38:00 -0600

    server.js notes added

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+//required files and packages.
 const express = require('express');
 const path = require('path');
 const api = require('./Routes/index.js');
 
+//Static port
 const PORT = 3001;
 
+//app to use express
 const app = express();
 
 app.use(express.json());
@@ -12,7 +15,7 @@ app.use('/api', api);
 
 app.use(express.static('public'))
 
-
+//application home page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -22,7 +25,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
-
+//console log when the port becomes active
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
