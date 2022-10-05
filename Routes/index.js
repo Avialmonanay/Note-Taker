faf605689a007c1605396186b68b76da67dbb71c
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-10-04 21:43:43 -0600

    routes/index.js comments added

--- Routes/index.js
+++ Routes/index.js
@@ -1,10 +1,12 @@
+//router index. This was added so implimenting future routes will be easier to link to the server.js
 const express = require('express');
 
 const notesRouter = require('./notes')
 
 const app = express();
 
-
+//uses the notes.js files API calls.
 app.use('/notes', notesRouter);
 
+//export as app
 module.exports = app;
