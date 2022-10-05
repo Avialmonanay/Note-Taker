f83f0835d5cc883d2f6a9bafadff9ea908d08694
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-10-04 21:33:33 -0600

    API Delete, Add, and Get all created and tested. Added additional Title Get

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,5 +1,6 @@
 const notes = require('express').Router();
 const { readAndAppend } = require('../helpers/fsUtils');
+const { writeToFile } = require('../helpers/fsUtils');
 const noteData = require('../db/db.json');
 const uuid = require('../helpers/uuid')
 
@@ -7,15 +8,36 @@ const uuid = require('../helpers/uuid')
 
 notes.get('/', (req, res) => res.json(noteData))
 
+notes.get('/:id', (req, res) => {
+    const noteID = req.params.id;
+    const note = noteData.find(_note => _note.id === noteID)
+    if (note) {
+        res.json(note)
+    }
+    else {
+        res.json({message: `Note ${noteID} not found`})
+    }
+})
+
+notes.get('/:title', (req, res) => {
+    const noteID = req.params.id;
+    const note = noteData.find(_note => _note.id === noteID)
+    if (note) {
+        res.json(note)
+    }
+    else {
+        res.json({message: `Note ${noteID} not found`})
+    }
+})
 
 
 notes.post('/', (req, res) => {
 
-    const {title, note} = req.body
-    if (title && note) {
+    const {title, text} = req.body
+    if (title && text) {
         const newNote = {
             title,
-            note,
+            text,
             note_Id: uuid(),
         }
         readAndAppend(newNote, './db/db.json');
@@ -27,8 +49,27 @@ notes.post('/', (req, res) => {
         res.json(response)
     }
     else {
-        res.json('Error in posting feedback');
+        res.json('Error in posting note');
       }
 })
 
-module.exports = notes;
+notes.delete('/:id' , (req, res) => {
+    const deleteNote = req.params.id;
+    const checkNote = noteData.find(_note => _note.id === deleteNote)
+    console.log(checkNote)
+    if (checkNote){
+    console.log("Delete item with id: ", deleteNote);
+ 
+    const filteredData = noteData.filter(note => note.id !== deleteNote);
+    res.status(204)
+
+    writeToFile('./db/db.json', filteredData )
+    
+}
+
+    else {
+        res.json ("ID does not exist")
+    }
+})
+
+module.exports = notes
